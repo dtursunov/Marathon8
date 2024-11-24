f7bdf625a4ce45a675ae259417059580a9db738f
Tursunov Diyor <diyor.9408@gmail.com>
2024-11-24 20:19:54 +0500

    first commit

---
+++ Marathon8/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  Marathon8
+//
+//  Created by Diyor Tursunov on 22/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UIScrollViewDelegate, UITableViewDelegate {
+    
+    let tableView = UITableView(
+        frame: .init(
+            origin: .zero,
+            size: .init(
+                width: UIScreen.main.bounds.width,
+                height: UIScreen.main.bounds.height
+            )),
+        style: .plain
+    )
+    
+    lazy var titleStackView: TitleStackView = {
+        let titleStackView = TitleStackView(frame: CGRect(origin: .zero, size: CGSize(width: view.bounds.width, height: 44.0)))
+        titleStackView.translatesAutoresizingMaskIntoConstraints = false
+        return titleStackView
+    }()
+    
+    lazy var tableHeaderView: UIView = {
+        let tableHeaderView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: view.bounds.width, height: 44.0)))
+        tableHeaderView.addSubview(titleStackView)
+        titleStackView.leadingAnchor.constraint(equalTo: tableHeaderView.leadingAnchor, constant: 16.0).isActive = true
+        titleStackView.topAnchor.constraint(equalTo: tableHeaderView.topAnchor).isActive = true
+        titleStackView.trailingAnchor.constraint(equalTo: tableHeaderView.trailingAnchor, constant: -16.0).isActive = true
+        titleStackView.bottomAnchor.constraint(equalTo: tableHeaderView.bottomAnchor).isActive = true
+        return tableHeaderView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        navigationController?.navigationBar.isTranslucent = false
+        navigationController?.navigationBar.barTintColor = UIColor.white
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.backgroundColor = .white
+        tableView.tableHeaderView = tableHeaderView
+
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.compactAppearance = navBarAppearance
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let maxTitlePoint = tableView.convert(
+            CGPoint(x: titleStackView.titleLabel.bounds.minX, y: titleStackView.titleLabel.bounds.maxY),
+            from: titleStackView.titleLabel
+        )
+        title = scrollView.contentOffset.y > maxTitlePoint.y ? "Avatar" : nil
+     }
+}
+
+
+class TitleStackView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        axis = .horizontal
+        alignment = .center
+        addArrangedSubview(titleLabel)
+        addArrangedSubview(button)
+    }
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: UIFont.preferredFont(forTextStyle: .largeTitle).pointSize, weight: .heavy)
+        label.text = "Avatar"
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var button: UIButton = {
+        let buttonWidth: CGFloat = 36.0
+        let button = UIButton(frame: CGRect(origin: .zero, size: CGSize(width: buttonWidth, height: buttonWidth)))
+        button.setImage(.init(systemName: "person.crop.circle.fill")?.imageWith(newSize: .init(width: buttonWidth, height: buttonWidth)), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: buttonWidth).isActive = true
+        button.heightAnchor.constraint(equalToConstant: buttonWidth).isActive = true
+        button.layer.masksToBounds = true
+        return button
+    }()
+}
+
+extension UIImage {
+    func imageWith(newSize: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: newSize).image { _ in
+            draw(in: CGRect(origin: .zero, size: newSize))
+        }
+        
+        return image.withRenderingMode(renderingMode)
+    }
+}
